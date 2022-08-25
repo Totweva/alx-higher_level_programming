a7a01aa2e6979babcb526e24e263f686d57bd668
Totweva <totweva@hmail.com>
2022-08-25 07:57:07 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
     if i == 0:
-        print("{} arguments.".format(i))
+        print("0 arguments.")
     elif i == 1:
-        print("{} argument".format(i))
+        print("1 argument")
     else:
         print("{} arguments:".format(i))
     for j in range(i):
