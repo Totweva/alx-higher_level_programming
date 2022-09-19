b83d0cf3a026283450c4e2bcd366d79d21de1c1a
Totweva <totweva@hmail.com>
2022-09-19 11:38:06 -0700

    added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """set the height of the rectangle."""
         return self.__height
 
     @height.setter
