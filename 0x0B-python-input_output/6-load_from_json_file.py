ab1ac383fb493f795071428ba0990a7ae2376b41
Totweva <totweva@hmail.com>
2022-09-27 06:21:58 -0700

    added commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,6 @@
 
 def load_from_json_file(filename):
     """creates an Object with a JSON file"""
-    with open(filename) as f:
-        return jsaon.load(f)
 
+    with open(filename) as f:
+        return json.load(f)
