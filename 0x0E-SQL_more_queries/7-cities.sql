9845e7db6c0d30f67260c0d0ea588114d7bc7a11
Totweva <totweva@hmail.com>
2022-11-02 09:21:00 -0700

    single final

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use the databases created
 USE hbtn_0d_usa;
---- creates table
+-- creates table
 CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
