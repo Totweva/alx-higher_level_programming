e7d2990cddc2595d39bffdfa645b27f77f38674b
Totweva <totweva@hmail.com>
2022-11-28 12:33:31 -0800

    adj

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let myVar = 'C is fun';
-let hyVar = 'Python is cool';
-let vyVar = 'JavaScript is amazing';
-console.log(myVar, hyVar, vyVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
