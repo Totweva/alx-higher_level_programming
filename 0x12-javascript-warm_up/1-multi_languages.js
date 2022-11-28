d209ac61b97670cfcf02e41c1c1a68217755fbab
Totweva <totweva@hmail.com>
2022-11-28 11:16:29 -0800

    first commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+let myVar = 'C is fun';
+let hyVar = 'Python is cool';
+let vyVar = 'JavaScript is amazing';
+console.log(myVar, hyVar, vyVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+Folder contains the first tasks on Javascript
