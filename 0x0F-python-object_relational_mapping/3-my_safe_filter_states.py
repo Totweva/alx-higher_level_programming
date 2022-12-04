052e63279f3c8b8f28b34ccb52e10428deaa9509
Totweva <totweva@hmail.com>
2022-12-04 03:25:10 -0800

    adjusted 4, 3 and 8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,46 @@
 #!/usr/bin/python3
+"""3-my_safe_filter_states module
+contains a script that lists all states with a given name
+from a given database and is sql-injection safe
 """
-Lists all values in the states tables of a database where name
-matches the argument in a safe way
-"""
-import sys
+from sys import argv
+
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
-    states = cur.fetchall()
+def list_all_states_filtered(username, password, db_name, state_name):
+    """lists all states with the given name from a database
+    Args:
+        username (str): mysql username
+        password (str): mysql password
+        db_name (str): the database name
+        state_name (str): the state name
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC",
+        [state_name]
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
-    for state in states:
-        print(state)
+
+if __name__ == "__main__":
+    if (len(argv) - 1 >= 4):
+        list_all_states_filtered(
+            username=argv[1],
+            password=argv[2],
+            db_name=argv[3],
+            state_name=argv[4]
+        )
