1a936ddd30b0823216f4616f10f403ae919f05bf
Kenneth Ojimba <totweva@gmail.com>
2023-01-18 19:17:49 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module contains defintion of class BaseGeometry
-"""
-
-
-class BaseGeometry:
-    """Base class for geometry operations
-    """
-    pass
--- 0x0A-python-inheritance/README.md
+++
@@ -1,1 +0,0 @@
-Solutions on the Python inheritance
