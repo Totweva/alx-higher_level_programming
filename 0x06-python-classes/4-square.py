8a6d1622bfd364132cd2d56f57ca1e7cd9697a22
Totweva <totweva@gmail.com>
2023-01-26 12:34:53 -0800

    add correction

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 """A Square class method"""
 
 
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size=0):
         """Initialize a new square.
@@ -16,13 +14,12 @@ class Square:
 
 
     @property
-    def size(size):
+    def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
