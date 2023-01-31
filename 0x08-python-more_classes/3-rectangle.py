9cc3e410f3a0f6e8cc339ac689def169ec776704
Totweva <totweva@gmail.com>
2023-01-31 14:25:26 -0800

    fourth

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -70,10 +70,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        _rec = ""
+        _rec = []
         for i in range(self.__height):
-            for j in range(self.__width):
-                _rec += "#"
+            [_rec.append("#") for j in range(self.__width)]
             if i != self.__height - 1:
-                _rec += "\n"
-        return _rec
+                _rec.append("\n")
+        return ("".join(_rec))
