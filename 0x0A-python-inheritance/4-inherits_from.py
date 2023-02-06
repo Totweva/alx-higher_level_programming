17674b6be0311bcaf5a470264269df9b162d0012
Totweva <totweva@gmail.com>
2023-02-06 13:39:43 -0800

    4th & 5th

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a is_same_class function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    a function that returns TRUE if the obj is an
+    instance that inherited from the specified class, 
+    and FALSE if it isnt
+    args:
+        obj: instance
+        a_class: class
+    """
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class BaseGeometry:
+    """initializing a class"""
+    pass
