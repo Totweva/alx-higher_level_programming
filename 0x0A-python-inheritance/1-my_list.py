2d345be54d2bec5489ce331f0b7ed723acf95989
Totweva <totweva@gmail.com>
2023-02-06 13:14:08 -0800

    re-first

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@ class MyList(list):
     """a class MyList that inherits from list"""
 
     def print_sorted(self):
-    """prints a list in ascending order"""
-    print(sorted(list))
+        """prints a list in ascending order"""
+        print(sorted(list))
