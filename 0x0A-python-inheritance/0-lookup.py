d4e264dac2d2cbc7855d0415d99dfe2a814222aa
Totweva <totweva@gmail.com>
2023-02-06 12:13:17 -0800

    first and second

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define a lookup function"""
+
+
+def lookup(obj):
+    """a function that lists all attrs and methods"""
+
+    return list.dir()
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""define the class MyList"""
+
+
+class MyList(list):
+    """a class MyList that inherits from list"""
+
+    list.__init__(self)
+
+    def print_sorted(self):
+    """prints a list in ascending order"""
+
