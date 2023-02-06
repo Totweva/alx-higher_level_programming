ed09bdca4d9074d5819ea11183892556fc8abd4e
Totweva <totweva@gmail.com>
2023-02-06 13:09:54 -0800

    third

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define a is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """
+    a function that returns TRUE if the obj is an
+    instance of the specified class, and FALSE if it isnt
+
+    args:
+        obj:
+        a_class:
+    """
+    x = a_class(dir(obj))
+    for i in range(x):
+       if i = obj:
+           return True
+       return False
