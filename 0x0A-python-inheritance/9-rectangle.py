9d46319fea47a127946311de8b3d2ed1352bff1b
Totweva <totweva@gmail.com>
2023-02-06 15:28:43 -0800

    9th

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,4 +25,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """print out as string"""
-        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
+        return (f"[{self.__class__.__name__}] {self.__width}/{self.__height}")
