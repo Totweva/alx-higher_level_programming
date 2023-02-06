10db07632ff8f1b3afaf6661751f5674339f1293
Totweva <totweva@gmail.com>
2023-02-06 13:51:18 -0800

    6th

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class BaseGeometry:
+    """initializing a class"""
+
+    def area(self):
+        """
+        defining the propety area
+        Raises:
+            Exception
+        """
+       raise Exception("area() is not implemented")
