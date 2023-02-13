44b4ba778d2673e78e8f039c3a8ba8a37a9afb40
Totweva <totweva@gmail.com>
2023-02-13 11:55:32 -0800

    first

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+class Base:
+    """ a base class"""
+    __nb_objects = 0
+
+
+    def __init__(self. id=None):
+        """
+        Initializing base class
+        Args:
+            id (int)
+        """
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects += 1
+            self.id = self.__nb_objects
