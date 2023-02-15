b8ae20c7f09d98fe162738c3398d207ca6831d71
Totweva <totweva@gmail.com>
2023-02-15 13:15:59 -0800

    fourth & fifth

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,9 +100,10 @@ class Rectangle(Base):
     def __str__(self):
         """string rep"""
         return ("[{}] ({}) {}/{} - {}/{}".format(
-            self.__class__.__name__
+            self.__class__.__name__,
             self.id,
             self.__x.
             self.__y,
             self.__width,
             self.__height)
+            )
